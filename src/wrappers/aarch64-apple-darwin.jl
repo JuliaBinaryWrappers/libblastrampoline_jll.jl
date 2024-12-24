2917263199e6e92789794245c05ea2f1e57702e8
jlbuild <juliabuildbot@gmail.com>
2024-12-24 16:11:02 +0000

    libblastrampoline_jll build 5.12.0+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -7,7 +7,7 @@ function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libblastrampoline,
-        "lib/libblastrampoline.5.4.0.dylib",
+        "lib/libblastrampoline.5.12.0.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
