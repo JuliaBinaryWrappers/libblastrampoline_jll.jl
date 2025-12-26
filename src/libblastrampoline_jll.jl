63743795ae446c9384596855a46d48017c416c8f
jlbuild <juliabuildbot@gmail.com>
2025-12-26 20:09:44 +0000

    libblastrampoline_jll build 5.11.2+2

--- src/libblastrampoline_jll.jl
+++ src/libblastrampoline_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("libblastrampoline")
-JLLWrappers.@generate_main_file("libblastrampoline", UUID("8e850b90-86db-534c-a0d3-1478176c7d93"))
+JLLWrappers.@generate_main_file("libblastrampoline", Base.UUID("8e850b90-86db-534c-a0d3-1478176c7d93"))
 end  # module libblastrampoline_jll
